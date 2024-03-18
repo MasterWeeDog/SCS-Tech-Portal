84d710e94a275c106926719b1b89ed852acc3808
MasterWeeDog <104869765+MasterWeeDog@users.noreply.github.com>
2024-03-18 08:47:51 -0400

    Update data.js

--- data.js
+++ data.js
@@ -5,64 +5,131 @@ const troubleshootingData = [
     },
     {
         issue: "Wash Pump Runs – Timer Will Not Cycle",
-        imagePath: "/SCS-Tech-Portal /CMA/Wash Pump Runs – Timer Will Not Cycle.PNG"
+        imagePath: "CMA/Wash Pump Runs – Timer Will Not Cycle.png"
     },
      {
         issue: "Machine Cycles, Normal Wash Pump Will Not Run",
-        imagePath: "/CMA/Machine Cycles, Normal Wash Pump Will Not Run.PNG"
+        imagePath: "CMA/Machine Cycles, Normal Wash Pump Will Not Run.png"
     },
      {
         issue: "Machine Will Not Turn Off",
-        imagePath: "SCS-Tech-Portal /CMA/Machine Will Not Turn Off.png"
+        imagePath: "CMA/Machine Will Not Turn Off.png"
     },
      {
         issue: "Dishwasher Will Not Drain, Drain Bill Not Lifting",
-        imagePath: "SCS-Tech-Portal /CMA/Dishwasher Will Not Drain, Drain Bill Not Lifting.PNG"
+        imagePath: "CMA/Dishwasher Will Not Drain, Drain Bill Not Lifting.png"
     },
      {
         issue: "Drain Will Not Close, Drain Ball Will Not Fall",
-        imagePath: "SCS-Tech-Portal /CMA/Drain Will Not Close, Drain Ball Will Not Fall.PNG"
+        imagePath: "CMA/Drain Will Not Close, Drain Ball Will Not Fall.png"
     },
      {
         issue: "Chemical Pumps Will Not Run",
-        imagePath: "SCS-Tech-Portal /CMA/Chemical Pumps Will Not Run.PNG"
+        imagePath: "CMA/Chemical Pumps Will Not Run.png"
     },
      {
         issue: "Machine Will Not Fill With Water – Machine Runs",
-        imagePath: "SCS-Tech-Portal /CMA/Machine Will Not Fill With Water – Machine Runs.PNG"
+        imagePath: "CMA/Machine Will Not Fill With Water – Machine Runs.png"
     },
      {
         issue: "Machine Only Fills When Fill Switch is Used",
-        imagePath: "SCS-Tech-Portal /CMA/Machine Only Fills When Fill Switch is Used.PNG"
+        imagePath: "CMA/Machine Only Fills When Fill Switch is Used.png"
     },
      { 
         issue:  "Machine Only Fills During The Wash Cycle",
-        imagePath: "SCS-Tech-Portal /CMA/Machine Only Fills During The Wash Cycle.PNG"
+        imagePath: "CMA/Machine Only Fills During The Wash Cycle.png"
     },
      {
     issue: "Fill Valve only Runs During the Wash Cycle",
-    imagePath: "SCS-Tech-Portal/CMA/Fill Valve only Runs During the Wash Cycle.PNG"
+    imagePath: "CMA/Fill Valve only Runs During the Wash Cycle.png"
     },
      {
         issue: "Machine Only Fills When Fill Switch is Used",
-        imagePath: "SCS-Tech-Portal /CMA/Machine Only Fills When Fill Switch is Used.PNG"
+        imagePath: "CMA/Machine Only Fills When Fill Switch is Used.png"
     },
      {
         issue: "Fill Water Will Not Shut Off",
-        imagePath: "SCS-Tech-Portal /CMA/Fill Water Will Not Shut Off.PNG"
+        imagePath: "CMA/Fill Water Will Not Shut Off.png"
     },
      {
         issue: "Wash Tank Heater Is Not Getting Hot",
-        imagePath: "SCS-Tech-Portal /CMA/Wash Tank Heater Is Not Getting Hot.PNG"
+        imagePath: "CMA/Wash Tank Heater Is Not Getting Hot.png"
     },
      {
         issue: "Booster Heat Not Getting Hot (Final Rinse)",
-        imagePath: "SCS-Tech-Portal /CMA/Booster Heat Not Getting Hot (Final Rinse).PNG"
+        imagePath: "CMA/Booster Heat Not Getting Hot (Final Rinse).png"
     }
 
 
 
-    // Add more issues and their corresponding images
+   // Add more issues and their corresponding images
+];
+
+// Copyright 2024  Leon W. T. Penningtonconst troubleshootingData = [
+    {
+        issue: "Machine Will Not Start",
+        imagePath: "CMA/Machine Will Not Start.png"
+    },
+    {
+        issue: "Wash Pump Runs – Timer Will Not Cycle",
+        imagePath: "/CMA/Wash Pump Runs – Timer Will Not Cycle.png"
+    },
+     {
+        issue: "Machine Cycles, Normal Wash Pump Will Not Run",
+        imagePath: "CMA/Machine Cycles, Normal Wash Pump Will Not Run.png"
+    },
+     {
+        issue: "Machine Will Not Turn Off",
+        imagePath: "CMA/Machine Will Not Turn Off.png"
+    },
+     {
+        issue: "Dishwasher Will Not Drain, Drain Bill Not Lifting",
+        imagePath: "CMA/Dishwasher Will Not Drain, Drain Bill Not Lifting.png"
+    },
+     {
+        issue: "Drain Will Not Close, Drain Ball Will Not Fall",
+        imagePath: "CMA/Drain Will Not Close, Drain Ball Will Not Fall.png"
+    },
+     {
+        issue: "Chemical Pumps Will Not Run",
+        imagePath: "CMA/Chemical Pumps Will Not Run.png"
+    },
+     {
+        issue: "Machine Will Not Fill With Water – Machine Runs",
+        imagePath: "CMA/Machine Will Not Fill With Water – Machine Runs.png"
+    },
+     {
+        issue: "Machine Only Fills When Fill Switch is Used",
+        imagePath: "CMA/Machine Only Fills When Fill Switch is Used.png"
+    },
+     { 
+        issue:  "Machine Only Fills During The Wash Cycle",
+        imagePath: "CMA/Machine Only Fills During The Wash Cycle.png"
+    },
+     {
+    issue: "Fill Valve only Runs During the Wash Cycle",
+    imagePath: "CMA/Fill Valve only Runs During the Wash Cycle.png"
+    },
+     {
+        issue: "Machine Only Fills When Fill Switch is Used",
+        imagePath: "CMA/Machine Only Fills When Fill Switch is Used.png"
+    },
+     {
+        issue: "Fill Water Will Not Shut Off",
+        imagePath: "CMA/Fill Water Will Not Shut Off.png"
+    },
+     {
+        issue: "Wash Tank Heater Is Not Getting Hot",
+        imagePath: "CMA/Wash Tank Heater Is Not Getting Hot.png"
+    },
+     {
+        issue: "Booster Heat Not Getting Hot (Final Rinse)",
+        imagePath: "CMA/Booster Heat Not Getting Hot (Final Rinse).png"
+    }
+
+
+
+   // Add more issues and their corresponding images
 ];
 
 // Copyright 2024  Leon W. T. Pennington
